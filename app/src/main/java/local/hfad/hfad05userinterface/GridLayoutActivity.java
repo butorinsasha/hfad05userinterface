2497fa234acd9fd559832f79479ea6fecf840106
a.b.butorin <a.b.butorin@tinkoff.ru>
2023-06-01 18:25:51 +0300

    Chapter 5. Exploring other Views: ImageButton

--- app/src/main/java/local/hfad/hfad05userinterface/GridLayoutActivity.java
+++ app/src/main/java/local/hfad/hfad05userinterface/GridLayoutActivity.java
@@ -1,5 +1,6 @@
 package local.hfad.hfad05userinterface;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -102,5 +103,11 @@ public class GridLayoutActivity extends AppCompatActivity {
                         ((RadioButton) findViewById(id)).getText() + " with id = " + id,
                         Toast.LENGTH_SHORT)
                 .show();
+    }
+
+    public void onImageButtonClicked(View view) {
+        Toast.makeText(this, "is onImageButtonClicked", Toast.LENGTH_SHORT).show();
+        Intent intent = new Intent(this, SecondLinearLayoutActivity.class);
+        startActivity(intent);
     }
 }
