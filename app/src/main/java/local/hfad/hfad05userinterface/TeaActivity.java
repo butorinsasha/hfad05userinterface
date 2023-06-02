18ecf552f3312cbe438b4bc8b0851eb52d845227
a.b.butorin <a.b.butorin@tinkoff.ru>
2023-06-02 20:49:11 +0300

    Chapter 5. Exercise TeaActivity

---
+++ app/src/main/java/local/hfad/hfad05userinterface/TeaActivity.java
@@ -0,0 +1,13 @@
+package local.hfad.hfad05userinterface;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TeaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tea);
+    }
+}
